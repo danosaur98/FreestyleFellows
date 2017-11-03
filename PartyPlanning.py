9aced56d432fbacd2ec764cfa0f490d7a50c1d87
Daniel Zhou <daniel.zhouo2012@gmail.com>
2017-11-03 03:10:33 -0400

    added item list data structure

--- PartyPlanning.py
+++ PartyPlanning.py
@@ -1,6 +1,32 @@
 # ASSUMPTIONS:
 # foods are not distinct by capital and lower case values
 # every drink/food requested in people.txt is found in drinks.txt/food.txt
+
+
+class ItemList(object):
+    def __init__(self, item_list, popularity, cost):
+        self.item_list = item_list
+        self.popularity = popularity
+        self.cost = cost
+
+    def __str__(self):
+        return str(self.item_list) + "\nTotal Popularity: " + str(self.popularity) + "\nCost: " + str(self.cost)
+
+
+def compute_popularity(item_list, popularity):
+    total = 0
+    for item in item_list:
+        total += popularity[item]
+    return total
+
+
+def compute_cost(item_list, cost):
+    total = 0
+    for item in item_list:
+        total += cost[item]
+    return total
+
+
 # load all the data
 with open('people.txt') as p:
     people_raw = p.readlines()
@@ -45,18 +71,12 @@ for line in drinks_raw:
 food_cost = {}
 for line in food_raw:
     food_cost[line.split(":")[0].lower()] = int(line.split(":")[1])
-'''print(people)
-print(drinks_popularity)
-print(drinks_cost)
-print(food_popularity)
-print(food_cost)
-'''
 
 # budget given
 budget = 12
 
-# splits budget into food and drink
-budget_food_proportion = 1
+# splits budget into money spent on food and drink
+budget_food_proportion = 0.75
 food_budget = int(budget_food_proportion * budget)
 drinks_budget = budget - food_budget
 
@@ -64,20 +84,6 @@ food_memo = [[[] for fcol in range(food_budget + 1)] for frow in range(len(food_
 drinks_memo = [[[] for dcol in range(drinks_budget + 1)] for drow in range(len(drinks_popularity) + 1)]
 food_list = list(food_popularity.keys())
 drinks_list = list(drinks_popularity.keys())
-
-
-def compute_popularity(item_list, popularity):
-    total = 0
-    for item in item_list:
-        total += popularity[item]
-    return total
-
-
-def compute_cost(item_list, cost):
-    total = 0
-    for item in item_list:
-        total += cost[item]
-    return total
 
 
 # dynamic programming method to optimize popularity and cost
@@ -94,30 +100,28 @@ def optimize(item_type, index, budget_remaining):
         l = drinks_list
 
     if index == len(
-            popularity) or budget_remaining == 0:  # if all the items have been examined or all the money has been spent, return 0
-        return []
+            popularity) or budget_remaining == 0:  # if all the items have been examined or all the money has been spent, return an empty list
+        return ItemList([], 0, 0)
     elif memo[index][budget_remaining]:  # if we have previously visited this case, return the answer
         return memo[index][budget_remaining]
     elif cost[l[index]] > budget_remaining:  # if this item costs too much, skip it
         memo[index][budget_remaining] = optimize(item_type, index + 1, budget_remaining)
         return memo[index][budget_remaining]
-    else:  # if this item fits in our budget, see whether it's better to buy the item or not
-        option_one = compute_popularity(optimize(item_type, index + 1, budget_remaining), popularity)
-        appended = optimize(item_type, index + 1, budget_remaining - cost[l[index]])
-        if cost[l[index]] < budget_remaining - compute_popularity(appended, cost):
-            appended.append(l[index])
-        option_two = compute_popularity(appended, popularity)
+    else:  # if this item fits in our budget, see whether it's better to buy the item or not depending on which option yields a higher popularity
+        ignore = optimize(item_type, index + 1, budget_remaining).popularity
+        bought = optimize(item_type, index + 1, budget_remaining - cost[l[index]])
+        if cost[l[index]] < budget_remaining - bought.cost:
+            bought.item_list.append(l[index])
+            bought.popularity = compute_popularity(bought.item_list, popularity)
+            bought.cost = compute_cost(bought.item_list, cost)
+        buy = bought.popularity
 
-        if option_one >= option_two:
+        if ignore >= buy:
             memo[index][budget_remaining] = optimize(item_type, index + 1, budget_remaining)
         else:
-            memo[index][budget_remaining] = appended
+            memo[index][budget_remaining] = bought
 
-        # print(type(optimize(item_type, index + 1, budget_remaining - cost[l[index]])))
         return memo[index][budget_remaining]
 
 
-print(food_list)
-print(food_popularity)
-print(food_cost)
 print(optimize("food", 0, food_budget))
