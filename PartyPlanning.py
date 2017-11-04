b60a3d42fd30594a13da718db77514fabc49f0e1
danosaur98 <daniel.zhou2012@gmail.com>
2017-11-03 22:41:44 -0400

    finished

--- PartyPlanning.py
+++ PartyPlanning.py
@@ -1,31 +1,62 @@
-# ASSUMPTIONS:
-# foods are not distinct by capital and lower case values
-# every drink/food requested in people.txt is found in drinks.txt/food.txt
+# dynamic programming method to optimize popularity and cost
+def optimize(item_type, index, budget_remaining):
+    if item_type == "food":
+        memo = food_memo
+        popularity = food_popularity
+        cost = food_cost
+        booleans = food_booleans
+        l = food_list
+    else:
+        memo = drinks_memo
+        popularity = drinks_popularity
+        cost = drinks_cost
+        l = drinks_list
+        booleans = drinks_booleans
+
+    if index == len(
+            l) or budget_remaining == 0:  # if all the items have been examined or all the money has been spent, return an empty list
+        # return ItemList([], 0, 0)
+        return 0
+    elif memo[index][budget_remaining]:  # if we have previously visited this case, return the answer
+        return memo[index][budget_remaining]
+    elif cost[l[index]] > budget_remaining:  # if this item costs too much, skip it
+        memo[index][budget_remaining] = optimize(item_type, index + 1, budget_remaining)
+        return memo[index][budget_remaining]
+    else:  # if this item fits in our budget, see whether it's better to buy the item or not depending on which option yields a higher popularity
+        ignore = optimize(item_type, index + 1, budget_remaining)
+        buy = popularity[l[index]] + optimize(item_type, index + 1, budget_remaining - cost[l[index]])
+        if buy > ignore:
+            booleans[index][budget_remaining] = True
+            memo[index][budget_remaining] = buy
+        else:
+            memo[index][budget_remaining] = ignore
+
+        return memo[index][budget_remaining]
 
 
-class ItemList(object):
-    def __init__(self, item_list, popularity, cost):
-        self.item_list = item_list
-        self.popularity = popularity
-        self.cost = cost
-
-    def __str__(self):
-        return str(self.item_list) + "\nTotal Popularity: " + str(self.popularity) + "\nCost: " + str(self.cost)
-
-
-def compute_popularity(item_list, popularity):
-    total = 0
-    for item in item_list:
-        total += popularity[item]
-    return total
-
-
-def compute_cost(item_list, cost):
-    total = 0
-    for item in item_list:
-        total += cost[item]
-    return total
-
+def get_items(item_type):
+    if item_type == "food":
+        memo = food_memo
+        popularity = food_popularity
+        cost = food_cost
+        l = food_list
+        booleans = food_booleans
+        budget = food_budget
+    else:
+        memo = drinks_memo
+        popularity = drinks_popularity
+        cost = drinks_cost
+        l = drinks_list
+        booleans = drinks_booleans
+        budget = drinks_budget
+    ret = []
+    total_cost = 0
+    for i in range(0, len(l)):
+        if booleans[i][budget] == True:
+            ret.append(l[i])
+            budget -= cost[l[i]]
+            total_cost += cost[l[i]]
+    return [ret, total_cost]
 
 # load all the data
 with open('people.txt') as p:
@@ -73,56 +104,36 @@ for line in food_raw:
     food_cost[line.split(":")[0].lower()] = int(line.split(":")[1])
 
 # budget given
-budget = 12
-
+budget = 16
 # splits budget into money spent on food and drink
-budget_food_proportion = 0
+budget_food_proportion = .75
+
 food_budget = int(budget_food_proportion * budget)
-drinks_budget = budget - food_budget
-
 food_memo = [[[] for fcol in range(food_budget + 1)] for frow in range(len(food_popularity) + 1)]
-drinks_memo = [[[] for dcol in range(drinks_budget + 1)] for drow in range(len(drinks_popularity) + 1)]
 food_list = list(food_popularity.keys())
+food_booleans = [[False for x in range(food_budget + 1)] for y in range(len(food_popularity) + 1)]
+
+print("Food Popularity: " + str(food_popularity))
+print("Food Cost: " + str(food_cost))
+print("Food Budget: " + str(food_budget))
+
+food_result = optimize("food", 0, food_budget)
+print("Food Popularity: " + str(food_result))
+food_bought, food_cost = get_items("food")
+print("Food Bought: " + str(food_bought))
+print("Food Cost: " + str(food_cost))
+
+drinks_budget = budget - food_cost # if there is extra budget left from food, add it to the drink budget
+drinks_memo = [[[] for dcol in range(drinks_budget + 1)] for drow in range(len(drinks_popularity) + 1)]
 drinks_list = list(drinks_popularity.keys())
+drinks_booleans = [[False for i in range(drinks_budget + 1)] for j in range(len(drinks_popularity) + 1)]
 
+print("Drinks Popularity: " + str(drinks_popularity))
+print("Drinks Cost: " + str(drinks_cost))
+print("Drinks Budget: " + str(drinks_budget))
 
-# dynamic programming method to optimize popularity and cost
-def optimize(item_type, index, budget_remaining):
-    if item_type == "food":
-        memo = food_memo
-        popularity = food_popularity
-        cost = food_cost
-        l = food_list
-    else:
-        memo = drinks_memo
-        popularity = drinks_popularity
-        cost = drinks_cost
-        l = drinks_list
-
-    if index == len(
-            popularity) or budget_remaining == 0:  # if all the items have been examined or all the money has been spent, return an empty list
-        return ItemList([], 0, 0)
-    elif memo[index][budget_remaining]:  # if we have previously visited this case, return the answer
-        return memo[index][budget_remaining]
-    elif cost[l[index]] > budget_remaining:  # if this item costs too much, skip it
-        memo[index][budget_remaining] = optimize(item_type, index + 1, budget_remaining)
-        return memo[index][budget_remaining]
-    else:  # if this item fits in our budget, see whether it's better to buy the item or not depending on which option yields a higher popularity
-        ignore = optimize(item_type, index + 1, budget_remaining).popularity
-        bought = optimize(item_type, index + 1, budget_remaining - cost[l[index]])
-        if cost[l[index]] < budget_remaining - bought.cost:
-            bought.item_list.append(l[index])
-            bought.popularity = compute_popularity(bought.item_list, popularity)
-            bought.cost = compute_cost(bought.item_list, cost)
-        buy = bought.popularity
-
-        if ignore >= buy:
-            memo[index][budget_remaining] = optimize(item_type, index + 1, budget_remaining)
-        else:
-            memo[index][budget_remaining] = bought
-
-        return memo[index][budget_remaining]
-
-
-#print("Food: " + str(optimize("food", 0, food_budget)))
-print("Drinks:" + str(optimize("drinks", 0, drinks_budget)))
+drinks_result = optimize("drinks", 0, drinks_budget)
+print("Drinks Popularity: " + str(food_result))
+drinks_bought, drinks_cost = get_items("drinks")
+print("Drinks Bought: " + str(drinks_bought))
+print("Drinks Cost: " + str(drinks_cost))
--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # FreeStyle Fellows Application 2018
 
-Solution to the problem statement:
+Problem statement:
 
 You are planning a party for an arbitrary number of people and you need to buy drinks and food. Assume you’re given a budget (integer) and 3 files: 'people.txt', 'drinks.txt', 'food.txt'.
 
 'people.txt' contains an arbitrary number of people with their preferred drinks and food; each entry will be 3 lines long (1st line: name (not unique), 2nd line: drinks (comma delimited, not sorted), 3rd line: food (comma delimited, not sorted).
 
-'drinks.txt' and 'food.txt' are arbitrarily large and each entry will be 1 line long in the following format: <drink or food name>:unit cost
+'drinks.txt' and 'food.txt' are arbitrarily large and each entry will be 1 line long in the following format: drink or food name:unit cost
 
 Design and implement an algorithm that reads in the files and selects what drinks and foods you should purchase within your budget based on the information in the files. Be creative in how you optimize for food and drinks based on the known preferences.
 
@@ -14,25 +14,93 @@ Your answer should include an explanation of your algorithm, test cases, and a s
 
 Feel free to use any data structures and packages or create your own. Submit all your files in a zipped directory (which can include a link to your GitHub repository of this challenge).
 
-## Algorithmic Design
-#Loading the Data
+# Algorithmic Design
+##Loading the Data
 First, all the appropriate data is loaded. ```people``` is a list of all the guests (not used to solve the problem).
-```drinks_popularity``` is a dictionary with each drink as a key and the total number of times its been requested as the respective value.
+```drinks_popularity``` is a dictionary with each drink as a key and the total number of times it has been requested as the respective value.
 ```drinks_cost``` is a dictionary with each drink as a key and its cost as the respective value.
 ```food_popularity``` and ```food_cost``` is the same as ```drinks_popularity``` and ```drinks_cost``` respectively, but for foods.
+```drinks_list``` and ```foods_list``` are the keys of their respective popularity dictionaries formatted as a list. This is to index each item, since you can't access dictionary keys by index. 
 
-#Dynamic Programming
+##Dynamic Programming
 Since the question is recursive in nature with several repeated states, dynamic programming is used to optimize the solution.
-Each entry in the memo table contains a data structure called ItemList, which contains the list of items, its total popularity, and total cost. This is to reduce the
-time spent recomputing the total popularity and total cost every time an entry is revisited.
 
 The budget is split into money spent on food and money spent on drinks. The current proportion of the budget spent on food is 75%, but this can be easily changed in the code.
-An algorithm is performed separately on each budget.
+The algorithm is performed separately on each budget. It is optimized so that if there is leftover money from buying all the food items,
+it is added to the drinks budget.
 
 A memo table is constructed for both drinks and foods. It is organized by item index for columns and the amount of money remaining for columns. For example,
-```food_memo[2][4]``` would return the best case scenario for when you're examining the 2nd food item with $4 left.
+```food_memo[2][4]``` would return the best case scenario for examining the 2nd food item in the list with $4 left.
+
+The method ```optimize``` takes in 3 parameters: ```item_type```, which is either drinks or foods,```index```,
+which is the index of the item list we are currently examining, and ```budget_remaining```, which is the amount of money we have left to spend.
+There are four possible outcomes:
+1. If all the items have been examined or all the money has been spent (```index=len(l)```, or 
+```budget_remaining==0```), return an empty list with cost and popularity 0.
+2. If the best situation for this item index and budget remaining has already been calculated 
+(```memo[index, budget_remaining]!= 0```), return it.
+3. If the cost of the item exceeds the budget remaining (```cost[l[index]] > budget_remaining```), skip this item and check the next item. 
+4. If the cost of the item doesn't exceed the budget remaining, choose the more favorable outcome of ignoring or buying it.
+
+
+Each entry in the memo table takes O(1) time to calculate. There are n*B possible states, where n is the total number of items in the list and 
+B is the budget given. Thus, the worst case time complexity of the algorithm is O(Bn).
 
 ## Assumptions
-
 * Every item requested in people.txt is given a cost in either food.txt or drinks.txt
 * Items are indistinguishable by upper/lowercase
+
+## Test Cases
+### Input
+Budget = 16.
+
+
+people.txt:
+```
+P1
+water,milk,soda
+pizza,chips,cake
+P2
+water,orange juice
+chips,cake,pizza
+P3
+water,beer
+cookies,cupcakes,pizza
+```
+food.txt:
+```
+pizza:10
+chips:4
+cake:5
+cookies:1
+cupcakes:1
+```
+drinks.txt:
+```
+water:1
+milk:2
+soda:3
+beer:5
+orange juice:2
+```
+### Output
+```
+Food Bought: ['chips', 'cake', 'cookies', 'cupcakes']
+Drinks Bought: ['water', 'milk', 'orange juice']
+```
+### Explanation
+```
+Food Popularity: {'pizza': 3, 'chips': 2, 'cake': 2, 'cookies': 1, 'cupcakes': 1}
+Food Cost: {'pizza': 10, 'chips': 4, 'cake': 5, 'cookies': 1, 'cupcakes': 1}
+Food Budget: 12
+```
+Although pizza is the most popular of all the food items, the maximum total popularity including pizza would be 5 after buying
+pizza, cookies, and cupcakes. However, the highest total popularity of all possible sets is 6 after buying chips, cake, cookies, and cupcakes.
+Since the total cost was only 11 and the food budget was 12, the extra dollar is added to the drinks budget.
+
+```
+Drinks Popularity: {'water': 3, 'milk': 1, 'soda': 1, 'orange juice': 1, 'beer': 1}
+Drinks Cost: {'water': 1, 'milk': 2, 'soda': 3, 'beer': 5, 'orange juice': 2}
+Drinks Budget:5
+```
+The highest total popularity is 6 after buying water, milk, and orange juice. 
